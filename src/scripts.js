e85b96c2a54bb62b2780ed1706a2f523a9982c18
Tudor Chirila <t.chirila@student.tudelft.nl>
2023-06-08 16:43:27 +0200

    added graph

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+// Create the script element
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://unpkg.com/vis-network/standalone/umd/vis-network.min.js';
+
+// Append the script element to the head or body
+document.head.appendChild(script);
+
+console.log("hello")
+      // create an array with nodes
