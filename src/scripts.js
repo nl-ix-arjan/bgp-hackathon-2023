a66054f73963c4446e64dfd6b636f20d197b5d45
Tudor Măgirescu <tudor.magirescu@gmail.com>
2023-06-08 16:48:59 +0200

    Added boilerplate GET request

---
+++ src/scripts.js
@@ -0,0 +1,20 @@
+function requestPathForIp(ips) {
+    var url = 'https://hackathon-2023.nl-ix.net:8443/?query=';
+    var headers = new Headers();
+    headers.append('X-ClickHouse-User', 'hackathon');
+    headers.append('X-ClickHouse-Key', 'NLix_HT_6jun23');
+
+    var options = {
+        method: 'GET',
+        headers: headers,
+    };
+
+    query = 'SHOW TABLES'; // change this in the future
+    fetch(url, options)
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error: ', error);
+        })
+}
